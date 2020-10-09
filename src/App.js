fdbdaf3b0d645f5f4ca593cf927a3159e561ca67
djviodes <djviodes26@gmail.com>
2020-10-09 08:50:42 -0700

    Character component written

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [character, setCharacter] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    const fetchCharacter = () => {
+      axios.get(`https://swapi.dev/api/people/1/`)
+        .then(resolve => {
+          setCharacter(resolve.data);
+        })
+        .catch(error => {
+          debugger
+        })
+    }
+    fetchCharacter()
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character name={character.name} birth={character.birth_year} homeworld={character.homeworld}></Character>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    background-color: black;
+`
+
+const StyledHeader = styled.h1`
+    color: white;
+`
+
+const StyledPara = styled.p`
+    color: white;
+`
+
+const Character = props => {
+    const { name, birth, homeworld } = props;
+
+    return (
+        <StyledDiv>
+            <StyledHeader>{name}</StyledHeader>
+            <StyledPara>Birth Year: {birth}</StyledPara>
+            <StyledPara>Place Of Birth: {homeworld}</StyledPara>
+        </StyledDiv>
+    )
+}
+
+export default Character
